85b3a338e1f467597d2e2341d6c98e37ebc50bd7
msyk <nii@msyk.net>
2020-06-20 16:30:55 +0900

    V.1.0.1: Remove undefined method in IM v6.

--- index.js
+++ index.js
@@ -8,63 +8,63 @@
  * https://github.com/INTER-Mediator/INTER-Mediator/blob/master/dist-docs/License.txt
  */
 IMParts_Catalog['codemirror'] = {
-    instanciate: function (parentNode) {
-        let newId = parentNode.getAttribute('id') + '-cm'
-        let newNode = document.createElement('TEXTAREA')
-        newNode.setAttribute('id', newId)
-        INTERMediatorLib.setClassAttributeToNode(newNode, '_im_codemirror')
-        parentNode.appendChild(newNode)
-        this.ids.push(newId)
+  instanciate: function (parentNode) {
+    let newId = parentNode.getAttribute('id') + '-cm'
+    let newNode = document.createElement('TEXTAREA')
+    newNode.setAttribute('id', newId)
+    newNode.setAttribute('class', '_im_codemirror')
+    parentNode.appendChild(newNode)
+    this.ids.push(newId)
 
-        parentNode._im_getComponentId = (function () {
-            let theId = newId
-            return function () {
-                return theId
-            }
-        })()
+    parentNode._im_getComponentId = (function () {
+      let theId = newId
+      return function () {
+        return theId
+      }
+    })()
 
-        parentNode._im_setValue = (function () {
-            let theId = newId
-            let self = IMParts_Catalog['codemirror']
-            return function (str) {
-                self.initialValues[theId] = str
-            }
-        })()
-    },
+    parentNode._im_setValue = (function () {
+      let theId = newId
+      let self = IMParts_Catalog['codemirror']
+      return function (str) {
+        self.initialValues[theId] = str
+      }
+    })()
+  },
 
-    ids: [],
-    initialValues: {},
-    mode: 'htmlmixed',
+  ids: [],
+  initialValues: {},
+  mode: 'htmlmixed',
 
-    finish: function () {
-        let targetId, targetNode, editor, i
-        let self = IMParts_Catalog['codemirror']
-        for (i = 0; i < self.ids.length; i++) {
-            targetId = self.ids[i]
-            targetNode = document.getElementById(targetId)
-            if (targetNode) {
-                editor = CodeMirror.fromTextArea(targetNode, {
-                    mode: self.mode,
-                    autoRefresh: true,
-                    lineNumbers: true,
-                    indentUnit: 4
-                })
-                editor.setValue(self.initialValues[targetId])
-                editor.on('change', function () {
-                    let nodeId = targetId
-                    return function (instance, obj) {
-                        IMLibUI.valueChange(nodeId)
-                    }
-                }())
-                targetNode._im_getValue = function () {
-                    let insideEditor = editor
-                    return function () {
-                        return insideEditor.getValue()
-                    }
-                }()
-            }
-        }
-        this.ids = []
-        this.initialValues = {}
+  finish: function () {
+    let targetId, targetNode, editor, i
+    let self = IMParts_Catalog['codemirror']
+    for (i = 0; i < self.ids.length; i++) {
+      targetId = self.ids[i]
+      targetNode = document.getElementById(targetId)
+      if (targetNode) {
+        editor = CodeMirror.fromTextArea(targetNode, {
+          mode: self.mode,
+          autoRefresh: true,
+          lineNumbers: true,
+          indentUnit: 4
+        })
+        editor.setValue(self.initialValues[targetId])
+        editor.on('change', function () {
+          let nodeId = targetId
+          return function (instance, obj) {
+            IMLibUI.valueChange(nodeId)
+          }
+        }())
+        targetNode._im_getValue = function () {
+          let insideEditor = editor
+          return function () {
+            return insideEditor.getValue()
+          }
+        }()
+      }
     }
+    this.ids = []
+    this.initialValues = {}
+  }
 }
